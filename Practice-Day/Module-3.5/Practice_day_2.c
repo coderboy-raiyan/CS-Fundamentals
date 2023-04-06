ef54a28a25a85b1aaad3c02fef4d89de8c3b9011
Raiyan <tajkierhaque@gmail.com>
2023-04-06 14:24:57 +0600

    Practice day 3.5 done [Practice_day_3.5]

---
+++ Practice-Day/Module-3.5/Practice_day_2.c
@@ -0,0 +1,14 @@
+# include <stdio.h>
+
+int main(){
+    int i = 0;
+    int n;
+    scanf("%d",  &n);
+    while (n >= i)
+    {
+        printf("Hello\n");
+        i++;
+    }
+    
+    return 0;
+}
