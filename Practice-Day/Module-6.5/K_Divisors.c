6b8056b4e6002e21d08339a783bdbefa7f399d53
Raiyan <tajkierhaque@gmail.com>
2023-04-11 14:42:01 +0600

    K_Divisor optimized solution finding [module-6.5]

--- Practice-Day/Module-6.5/K_Divisors.c
+++ Practice-Day/Module-6.5/K_Divisors.c
@@ -1,15 +1,48 @@
-#include <stdio.h>
-int main ()
-{
-    int n;
-    scanf("%d", &n);
+# include <stdio.h>
+# include <math.h>
 
-    for (int i = 1; i <= n; i++)
-    {
-        if(n % i == 0){
-            printf("%d\n", i);
-        }
-    }
+// O(n) -> takes more time
+// int main ()
+// {
+//     int n;
+//     scanf("%d", &n);
+
+//     for (int i = 1; i <= n; i++)
+//     {
+//         if(n % i == 0){
+//             printf("%d\n", i);
+//         }
+//     }
     
+//     return 0;
+// }
+
+int main(){
+    int n, i = 2, j, limit;
+    scanf("%d", &n);
+    limit = (int)sqrt(n);
+    printf("%d\n", 1);
+    while (i <= limit)
+    {
+       j = 1;
+       int temp;
+       while (j <= 10)
+       {
+        if(i * j == n){
+            if(j == i){
+                printf("%d\n", i);
+            }else{
+                printf("%d\n%d\n", i, j);
+                
+            }
+        }
+        j++;
+       }
+       i++;
+    }
+    if(n > 10 && n < 100){
+        printf("%d\n", j-1);
+    }
+     printf("%d\n", n);
     return 0;
 }
