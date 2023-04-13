399c0cb5d1315600ae549f5bd5fadbc8f972a7fa
Raiyan <tajkierhaque@gmail.com>
2023-04-13 23:03:17 +0600

    Todays problems has been solved [moduel-7.5]

---
+++ index.c
@@ -0,0 +1,20 @@
+# include <stdio.h>
+
+int main(){
+    int n;
+    scanf("%d", &n);
+    int nums[n];
+    for (int i = 0; i < n; i++)
+    {
+       scanf("%d", &nums[i]);
+    }
+    while (n--)
+    {
+       if (n % 2 == 0){
+        printf("%d ", nums[n]);
+       }
+    }
+    
+    
+    return 0;
+}
