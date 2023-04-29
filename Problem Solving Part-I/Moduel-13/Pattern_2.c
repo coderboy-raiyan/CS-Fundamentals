52fdd16ff9f4fe4f9b76f4d8fe0d0fee8e0aced7
Raiyan <tajkierhaque@gmail.com>
2023-04-30 00:31:24 +0600

    pyramid reverse patter done [module-13]

--- Problem Solving Part-I/Moduel-13/Pattern_2.c
+++ Problem Solving Part-I/Moduel-13/Pattern_2.c
@@ -23,3 +23,28 @@ int main(){
     
     return 0;
 }
+
+// Reverse pattern
+int main(){
+    int n;
+    scanf("%d", &n);
+    int s = 0, k= (2*n) - 1; // just calculate the last column using (2 * n) -1 rules
+    for (int i = 0; i < n; i++)
+    {
+      for (int j = 0; j < s; j++)
+      {
+        printf(" ");
+      }
+      for (int j = 0; j < k; j++)
+      {
+        printf("*");
+      }
+      
+      s++;
+      k-=2;
+      printf("\n");
+      
+    }
+    
+    return 0;
+}
