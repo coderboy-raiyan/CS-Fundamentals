6059e7dd82990c1ce73cfb74b8f5e906e893cecd
Raiyan <tajkierhaque@gmail.com>
2023-04-30 01:56:50 +0600

    diamond pattern-4 solved [module-13]

---
+++ Problem Solving Part-I/Moduel-13/Pattern_3.c
@@ -0,0 +1,33 @@
+# include <stdio.h>
+
+int main(){
+    int n;
+    scanf("%d", &n);
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+           printf("%d ", j);
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}
+
+int main(){
+    int n;
+    scanf("%d", &n);
+    int k = n;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = k; j >= 1; j--)
+        {
+           printf("%d ", j);
+        }
+        k--;
+        printf("\n");
+    }
+    
+    return 0;
+}
