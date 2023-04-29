99b28466591cadd85d4599fb26cbaa49d1f3b3b7
Raiyan <tajkierhaque@gmail.com>
2023-04-30 00:18:48 +0600

    pyramid pattern done using nested loop [module-13]

---
+++ Problem Solving Part-I/Moduel-13/Pattern_2.c
@@ -0,0 +1,25 @@
+# include <stdio.h>
+
+int main(){
+    int n;
+    scanf("%d", &n);
+    int s = n-1, k=0; 
+    for (int i = 0; i < n; i++)
+    {
+      for (int j = 0; j < s; j++)
+      {
+        printf(" ");
+      }
+      for (int j = 0; j <= k; j++)
+      {
+        printf("*");
+      }
+      
+      s--;
+      k+=2;
+      printf("\n");
+      
+    }
+    
+    return 0;
+}
