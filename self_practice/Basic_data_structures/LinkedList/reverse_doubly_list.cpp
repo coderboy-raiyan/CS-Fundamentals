65329641fd7b01e378ea0a5d94205558bba7aad6
Raiyan <tajkierhaque@gmail.com>
2023-07-22 12:38:44 +0600

    LinkedList half practice done

---
+++ self_practice/Basic_data_structures/LinkedList/reverse_doubly_list.cpp
@@ -0,0 +1,148 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node *next;
+    Node *prev;
+
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+        this->prev = NULL;
+    };
+};
+
+class DoublyList
+{
+public:
+    Node *head = NULL;
+    Node *tail = NULL;
+    int sz = 0;
+
+    void insert_tail(int val)
+    {
+        sz++;
+        Node *newNode = new Node(val);
+        if (head == NULL)
+        {
+            head = newNode;
+            tail = newNode;
+            return;
+        };
+        tail->next = newNode;
+        newNode->prev = tail;
+        tail = newNode;
+    };
+    void insert_head(int val)
+    {
+        sz++;
+        Node *newNode = new Node(val);
+        if (head == NULL)
+        {
+            head = newNode;
+            tail = newNode;
+            return;
+        };
+        newNode->next = head;
+        head->prev = newNode;
+        head = newNode;
+    };
+    void insert_at_position(int pos, int val)
+    {
+        sz++;
+        Node *newNode = new Node(val);
+        Node *temp = head;
+        int i = 0;
+        while (i < pos - 1)
+        {
+            temp = temp->next;
+            i++;
+        }
+
+        newNode->next = temp->next;
+        temp->next->prev = newNode;
+        newNode->prev = temp;
+        temp->next = newNode;
+    };
+    void delete_head()
+    {
+        sz--;
+        Node *temp = head;
+        head = head->next;
+
+        if (head == NULL)
+        {
+            tail = NULL;
+        }
+        else
+        {
+            head->prev = NULL;
+        };
+
+        delete temp;
+    };
+    void delete_tail()
+    {
+        sz--;
+        Node *temp = tail;
+        tail = tail->prev;
+        if (tail == NULL)
+        {
+            head = NULL;
+        }
+        else
+        {
+            tail->next = NULL;
+        };
+        delete temp;
+    };
+    void reverse_list()
+    {
+        Node *l = head;
+        Node *r = tail;
+
+        while (l != r && l->next != r)
+        {
+            swap(l->val, r->val);
+
+            l = l->next;
+            r = r->prev;
+        }
+        if (sz % 2 == 0)
+        {
+            swap(l->val, r->val);
+        }
+    };
+    void print_list()
+    {
+        Node *temp = head;
+        while (temp != NULL)
+        {
+            cout << temp->val << " ";
+            temp = temp->next;
+        };
+    }
+};
+
+int main()
+{
+    DoublyList myList;
+    while (true)
+    {
+        int val;
+        cin >> val;
+        if (val == -1)
+        {
+            break;
+        };
+        myList.insert_tail(val);
+    }
+    myList.reverse_list();
+    myList.print_list();
+    return 0;
+}
