b633fabea9f42c830c4ecc4944882776e6721b16
Raiyan <tajkierhaque@gmail.com>
2023-08-07 17:25:12 +0600

    DFS and graph done

---
+++ Semester_2/Introduction_to_Algorithms/Module_6/DFS.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const int N = 1e5 + 5;
+vector<int> adj[N];
+
+bool visited[N];
+
+void dfs(int u)
+{
+    visited[u] = true;
+    cout << "visiting node : " << u << endl;
+    for (int v : adj[u])
+    {
+        if (visited[v])
+        {
+            continue;
+        };
+        dfs(v);
+    };
+}
+
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+    for (int i = 0; i < m; i++)
+    {
+        int u, v;
+        cin >> u >> v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    };
+    dfs(1);
+    // for (int i = 1; i <= n; i++)
+    // {
+    //     cout << "List " << i << " : ";
+    //     for (int it : adj[i])
+    //     {
+    //         cout << it << " ";
+    //     };
+    //     cout << endl;
+    // }
+
+    return 0;
+}
---
+++ Semester_2/Introduction_to_Algorithms/Module_6/depth_count.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const int N = 1e5 + 5;
+vector<int> adj[N];
+
+bool visited[N];
+int depth[N];
+
+void dfs(int u)
+{
+    visited[u] = true;
+    for (int v : adj[u])
+    {
+        if (visited[v])
+        {
+            continue;
+        };
+        depth[v] = depth[u] + 1;
+        dfs(v);
+    };
+}
+
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+    for (int i = 0; i < m; i++)
+    {
+        int u, v;
+        cin >> u >> v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    };
+    dfs(1);
+    for (int i = 1; i <= n; i++)
+    {
+        cout << "Depth node " << i << " : " << depth[i] << endl;
+    }
+
+    return 0;
+}
