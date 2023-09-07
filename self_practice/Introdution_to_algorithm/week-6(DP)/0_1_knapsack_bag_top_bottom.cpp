90c6c8fcf9af263d4822f5be7ea3bd263b0b0175
Raiyan <tajkierhaque@gmail.com>
2023-09-07 14:36:28 +0600

    half practice problem done

--- self_practice/Introdution_to_algorithm/week-6(DP)/0_1_knapsack_bag_top_bottom.cpp
+++ self_practice/Introdution_to_algorithm/week-6(DP)/0_1_knapsack_bag_top_bottom.cpp
@@ -46,26 +46,10 @@ int main()
     {
         for (int j = 0; j <= s; j++)
         {
-            if (i == 0 || j == 0)
-            {
-                dp[i][j] = 0;
-            }
-        }
-    };
-    for (int i = 1; i <= n; i++)
-    {
-        for (int j = 1; j <= s; j++)
-        {
-            if (w[i - 1] <= j)
-            {
-                dp[i][j] = max(dp[i - 1][j - w[i - 1]] + a[i - 1], dp[i - 1][j]);
-            }
-            else
-            {
-                dp[i][j] = dp[i - 1][j];
-            }
+            dp[i][j] = -1;
         }
     }
-    cout << dp[n][s];
+
+    cout << knapsack(a, w, n, s);
     return 0;
 }
--- self_practice/Introdution_to_algorithm/week-6(DP)/U_Knapsack.cpp
+++ self_practice/Introdution_to_algorithm/week-6(DP)/U_Knapsack.cpp
@@ -25,47 +25,24 @@ int knapsack(int v[], int w[], int n, int s)
     {
         return dp[n][s] = knapsack(v, w, n - 1, s);
     }
-}
+};
 
 int main()
 {
-    int n;
-    cin >> n;
-    int a[n], w[n];
+    int n, s;
+    cin >> n >> s;
+    int v[n], w[n];
     for (int i = 0; i < n; i++)
     {
-        cin >> a[i];
+        cin >> w[i] >> v[i];
     };
-    for (int i = 0; i < n; i++)
-    {
-        cin >> w[i];
-    };
-    int s;
-    cin >> s;
     for (int i = 0; i <= n; i++)
     {
         for (int j = 0; j <= s; j++)
         {
-            if (i == 0 || j == 0)
-            {
-                dp[i][j] = 0;
-            }
-        }
-    };
-    for (int i = 1; i <= n; i++)
-    {
-        for (int j = 1; j <= s; j++)
-        {
-            if (w[i - 1] <= j)
-            {
-                dp[i][j] = max(dp[i - 1][j - w[i - 1]] + a[i - 1], dp[i - 1][j]);
-            }
-            else
-            {
-                dp[i][j] = dp[i - 1][j];
-            }
+            dp[i][j] = -1;
         }
     }
-    cout << dp[n][s];
+    cout << knapsack(v, w, n, s);
     return 0;
 }
